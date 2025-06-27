fd35e44fba7bb799ba7af00c78497253aa77b089
Ajutate <33647141+yoneljorge@users.noreply.github.com>
2025-06-26 23:45:20 -0400

    Comienzo del curso desde cero del canal la Gueekipedia de Ernesto.

---
+++ curso_desde_cero-la_gueekipedia_de_ernesto/004-manipulacion_de_cadenas.py
@@ -0,0 +1,9 @@
+mensaje = "Hola Yonel"
+
+'''
+La Búsqueda: consiste en localizar dentro de una cadena, una subcadena más pequeña a un carácter.
+
+Para lo caul es necesario utilizar el método find.
+'''
+buscar_subcadena = mensaje.find("Yonel")
+print(buscar_subcadena)
